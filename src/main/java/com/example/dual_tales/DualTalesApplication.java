48c000f1c3c2e5e180bf86935f5581e94c4a747c
sebin0204 <pjjanguou@gmail.com>
2026-04-29 15:15:22 +0900

    Initial Commit : 프로젝트 기본 세팅

---
+++ src/main/java/com/example/dual_tales/DualTalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dual_tales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualTalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DualTalesApplication.class, args);
+	}
+
+}
